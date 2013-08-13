9ad2be2378c10089b10d396b3f195d898dcbf901
Felipe Cuevas <cuevas_castillo@hotmail.com>
2013-08-13 14:40:43 -0500

    Implementar el metodo listener para el boton y pasar parametros del imput box a la nueva clase ...Saludo.java

--- src/com/example/holausuario/MainActivity.java
+++ src/com/example/holausuario/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.holausuario;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,34 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Referencia a controles de interface 
+		final EditText txtNombre = (EditText)findViewById(R.id.TxtNombre);
+	    final Button btnHola = (Button)findViewById(R.id.BtnHola);
+		
+		//Implementamos el evento “click” del botón
+	    btnHola.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//Creamos el Intent
+                Intent intent =
+                        new Intent(MainActivity.this, FrmSaludo.class);
+
+                //Creamos la información a pasar entre actividades
+                Bundle b = new Bundle();
+                b.putString("NOMBRE", txtNombre.getText().toString());
+
+                //Añadimos la información al intent
+                intent.putExtras(b);
+
+                //Iniciamos la nueva actividad
+                startActivity(intent);
+				
+			}
+		});
+	
+		
 	}
 
 	@Override
